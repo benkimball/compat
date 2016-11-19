ffcffe8517ed1159b39414ec5ed1d59378bdc43f
Isaac Duran <isaax@utexas.edu>
2016-11-18 18:40:00 -0600

    changed the url for socket io

--- app/assets/javascripts/liveoak_compat/compatibility_test.js
+++ app/assets/javascripts/liveoak_compat/compatibility_test.js
@@ -44,7 +44,7 @@ tests.push({
     var deferred = Q.defer(), ws;
     var payload = "HELLO";
     try {
-      ws = io("ws://52.26.139.74:8080/", {
+      ws = io("wss://phabricator.liveoak.net/", {
         reconnection: true,
         reconnectionDelay: 1000,
         reconnectionDelayMax: 5000,
