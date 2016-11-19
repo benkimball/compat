045b09828934bd81035cb65618f236e686a30937
Isaac Duran <isaax@utexas.edu>
2016-11-18 21:28:02 -0600

    Updated gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
 
-  config.action_cable.allowed_request_origins = [ 'https://test.liveoak.net' ]
+  config.action_cable.allowed_request_origins = [ 'https://test.liveoak.net',
+                                                  'https://secure.liveoak.net',
+                                                  'https://secureus.liveoak.net',
+                                                  'https://loa-staging.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
